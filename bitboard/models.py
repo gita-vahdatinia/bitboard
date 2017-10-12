ed50d262f15e951c6e064fe631e0d94483d156e3
Joshua Stern <joshuastern@Joshuas-MacBook-Pro.local>
2017-10-12 11:43:31 -0700

    updated model, migrated db

--- bitboard/models.py
+++ bitboard/models.py
@@ -6,8 +6,22 @@ from django.db import models
 # Create your models here.
 
 class Cryptocurrency(models.Model):
-    name = models.CharField(max_length = 100)
-    symbol = models.CharField(max_length = 10)
+    tag = models.CharField(max_length=25, null=True)
+    name = models.CharField(max_length = 25, null=True)
+    symbol = models.CharField(max_length = 10, null=True)
+    rank = models.IntegerField(null=True)
+    price_usd = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    price_btc = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    volume_24h_usd = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    market_cap_usd = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    availabe_supply = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    total_supply = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    percent_change_1h = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    percent_change_24h = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    percent_change_7d = models.DecimalField(max_digits = 20, decimal_places = 2, null=True)
+    last_updated = models.IntegerField(null = True)
+
+
 
     def __str__(self):
         return self.name
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 python manage.py makemigrations
-python manage.py sqlmigrate bitboard 0001
 python manage.py migrate
 echo "from django.contrib.auth.models import User;\
       User.objects.filter(email='admin@example.com').delete();\
-      User.objects.create_superuser('admin', 'bitboard@gmail.com', 'fall2017')"\
+      User.objects.create_superuser('admin', 'admin@example.com', 'fall2017')"\
       | python manage.py shell
+
 exec "$@"
