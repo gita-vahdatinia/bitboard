9b1fc87f0a007d7ab7860e55ea53022b569b257b
Matthew Cruz <matthewcruz@eduroam-169-233-250-16.ucsc.edu>
2017-11-11 15:04:04 -0800

    working on login stuff

--- bitboard/urls.py
+++ bitboard/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+
+
+
 
 urlpatterns = [
     # /
@@ -14,5 +18,10 @@ urlpatterns = [
 
     # /cryptocurrency/bitcoin/
     url(r'^(cryptocurrency/)(?P<token_tag>[A-Za-z0-9_-]+)/$', views.token, name='token'),
+
+    # login stuff
+    url(r'^login/$', auth_views.login, name='login'),
+
+    url(r'^logout/$', auth_views.logout, name='logout'),
 
 ]
