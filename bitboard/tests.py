7580c5319a0d4c4b93bbd1285d3b38cba5b1c4bd
Guita Vahdatinia <guitapardis@yahoo.com>
2017-12-03 17:33:17 -0800

    Added more tests to tests.py for unit testing

--- bitboard/tests.py
+++ bitboard/tests.py
@@ -1,13 +1,37 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.test import TestCase
 
 from .models import News
+from .models import Profile
+
 
 class NewsModelTest(TestCase):
     def test_string_representation(self):
         news = News(title="7 Tokens Investors Are Talking About")
         news.save()
         self.assertEqual(str(news), news.title)
+
+        # -*- coding: utf-8 -*-
+
+
+
+class ProfileModelTest(TestCase):
+    def test_name(self):
+        name = Profile(user="TestGuyPerson")
+        name.save()
+        self.assertEqual(str(name), name.user)
+    def test_bio(self):
+        bio = Profile(bio="I'm a cool guy")
+        bio.save()
+        self.assertEqual(str(bio), bio.bio)
+    def test_location(self):
+        location = Profile(location="Los Angeles")
+        location.save()
+        location.assertEqual(str(location), location.location)
+    def test_birth(self):
+        birth = Profile(birth_date="November 6")
+        birth.save()
+        self.assertEqual(str(birth), birth.birth_date)
 # Create your tests here.
